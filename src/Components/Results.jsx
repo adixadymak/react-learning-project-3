d6995d3210b5bdb7b2d453f8735ecc31bdd9dfe1
adixadymak <adixadymak@gmail.com>
2024-07-29 10:10:08 +0100

    Feature: Outputting the result more professionally

--- src/Components/Results.jsx
+++ src/Components/Results.jsx
@@ -1,8 +1,11 @@
-import { calculateInvestmentResults } from "../util/investment.js";
+import { calculateInvestmentResults, formatter } from "../util/investment.js";
 
 export default function Results({ input }) {
   const investmentResults = calculateInvestmentResults(input);
-
+  const initialInvestment =
+    investmentResults[0].valueEndOfYear -
+    investmentResults[0].interest -
+    investmentResults[0].annualInvestment;
   console.log(investmentResults);
 
   return (
@@ -12,18 +15,28 @@ export default function Results({ input }) {
           <th>Year</th>
           <th>Investment Value</th>
           <th>Interest(Year)</th>
+          <th>Total Interest</th>
           <th>Invested Capital</th>
         </tr>
       </thead>
       <tbody>
-        {investmentResults.map((result) => (
-          <tr>
-            <td>{result.year}</td>
-            <td>{result.annualInvestment}</td>
-            <td>{result.interest}</td>
-            <td>{result.valueEndOfYear}</td>
-          </tr>
-        ))}
+        {investmentResults.map((result) => {
+          const totalInvestment =
+            result.valueEndOfYear -
+            result.annualInvestment * result.year -
+            initialInvestment;
+
+          const totalAmountInvested = result.valueEndOfYear - totalInvestment;
+          return (
+            <tr key={result.year}>
+              <td>{result.year}</td>
+              <td>{formatter.format(result.valueEndOfYear)}</td>
+              <td>{formatter.format(result.interest)}</td>
+              <td>{formatter.format(totalInvestment)}</td>
+              <td>{formatter.format(totalAmountInvested)}</td>
+            </tr>
+          );
+        })}
       </tbody>
     </table>
   );
